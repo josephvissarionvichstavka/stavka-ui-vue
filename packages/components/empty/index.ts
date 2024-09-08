a1693601ac3d2d81f28c41c7ef971afd126294cc
josephvissarionvichstavka <105615844+josephvissarionvichstavka@users.noreply.github.com>
2024-09-08 13:11:15 +0800

    Add files via upload

---
+++ packages/components/empty/index.ts
@@ -0,0 +1,6 @@
+import Empty from './src/index'
+import {withInstall} from "../../utils/install";
+
+const StEmpty = withInstall(Empty);
+
+export default StEmpty;
---
+++ packages/components/empty/src/index.ts
@@ -0,0 +1,39 @@
+import {defineComponent , h} from 'vue'
+const defaultEmpty = h('svg', {width : 100 , height : 100 } , [
+    h('polygon' , {points : '20,60 40,40 40,80 20,100', fill : '#6e6c6f'}),
+    h('rect', {x : 40 , y :40 , width : 40 , height : 40 ,fill : '#807e81'}),
+    h('polygon' , {points : '60,60 80,40 80,80 60,100' , fill : '#6e6c6f'}),
+    h('polygon' , {points : '60,60 80,40 95,45 75,65' , fill : '#c7c4c9'}),
+    h('rect', {x : 20 , y : 60 , width : 40 , height : 40 , fill : '#807e81'}),
+    h('polygon' , {points : '20,60 60,60 50,75 10,75' , fill : '#c7c4c9'}),
+])
+const StEmpty = defineComponent({
+    name : 'st-empty',
+    props : {
+        image : {
+            type : String,
+        },
+        imageSize : {
+            type : String,
+        },
+        description : String
+    },
+    setup(props) {
+        const styles = {
+            width: props.image ? props.imageSize ? props.imageSize : '100px' : '',
+            height : props.image ? props.imageSize ? props.imageSize : '100px' : ''
+        }
+        return {
+            styles
+        }
+    },
+    render () {
+        return h('div' , {class : 'st-empty' ,style : this.styles } , [
+            this.$props.image ? h('img' , {src : this.$props.image}) : defaultEmpty,
+            h('div' , {class : 'st-empty--description'} , this.$props.description || 'empty')
+        ])
+    }
+})
+
+
+export default StEmpty;
